d2d54f74b8d271297f0251fba8c41e9fb6c9d436
Rasul Iskandarov <73297277+rasul-isk@users.noreply.github.com>
2023-04-16 13:36:54 +0300

    fixed styling and carousel problems

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,15 +1,21 @@
 import { Box } from '@mui/material';
 import React from 'react';
-
+import { DefaultPlayer as Video } from 'react-html5video/dist';
+import 'react-html5video/dist/styles.css';
 export const VideoPlayer = ({ src }) => {
   //width="100%"
   // const finalPath =
+  //space - pause | f - fullscren | m - mute
   return (
-    <Box width="100vw" height="80vh">
-      <video controls width="100%" height="100%" key={src}>
-        <source src={`/video/${src}.mp4`} type="video/mp4" />
+    <Box width="100vw" height="80vh" display="flex" flexWrap="wrap" alignContent="center" justifyContent="center" margin="auto">
+      {/* <video controls width="100%" height="100%" key={src}>
         Sorry, your browser doesn't support videos.
-      </video>
+      </video> */}
+      <Box sx={{ width: '69.4vw', height: 'inherit' }}>
+        <Video ratio={16 / 9} key={src} poster={`/pics/${src}.png`}>
+          <source src={`/video/${src}.mp4`} type="video/mp4" />
+        </Video>
+      </Box>
     </Box>
   );
 };
--- src/components/VideoSlider.js
+++ src/components/VideoSlider.js
@@ -4,7 +4,6 @@ import { Box } from '@mui/material';
 import React from 'react';
 
 export const VideoSlider = ({ sources, content, setContent }) => {
-  
   const elements = [];
   const nextContent = () => {
     let index = sources.indexOf(content) + 1;
@@ -15,18 +14,23 @@ export const VideoSlider = ({ sources, content, setContent }) => {
     setContent(sources[index >= 0 ? index : 7]);
   };
 
-  sources.map((el) => {
-    //add focus on video by increasing slightly size of preview image and some other effects
-    //form object with onClick triger to setContent
-    //add style "inFocus ? make it more bold than other videos"
-    elements.push(<img key={el} src={`/pics/${el}.png`} className={el === content ? 'preview highlight' : 'preview'} ratio={16 / 9} height="100vh" onClick={() => setContent(el)} alt={el} />);
+  sources.foreach((el) => {
+    let index = sources.indexOf(content);
+    let style = el === content ? 'preview highlight' : 'preview preview-small';
+    style += index > 4 ? ' scroll-right' : '';
+    elements.push(<img key={el} src={`/pics/${el}.png`} className={style} ratio={16 / 9} height="100vh" onClick={() => setContent(el)} alt={el} />);
   });
+
+  //TODO arrows to separate icon component
+  //TODO clean code, remove unnecessary styles, add components to another components where it is needed
   return (
     <Box sx={{ padding: '0px 80px', display: 'block', height: '20vh', width: '100vw', borderBottom: '2px solid rgb(111, 29, 166)' }}>
-      <Box sx={{ display: 'inline-flex', fontSize: '10vh', height: '100%', width: '100%', justifyContent: 'space-between', alignItems: 'center' }}>
-        <ArrowBackIosIcon fontSize="10vh" onClick={() => prevContent()} />
-        <Box sx={{ display: 'inline-flex', fontSize: '10vh', justifyContent: 'space-between', alignItems: 'center' }}>{elements.length && elements}</Box>
-        <ArrowForwardIosIcon fontSize="10vh" onClick={() => nextContent()} />
+      <Box sx={{ display: 'flex', fontSize: '10vh', height: '100%', width: '100%', justifyContent: 'space-between', alignItems: 'center' }}>
+        <ArrowBackIosIcon sx={{ cursor: 'pointer', display: 'block', fill: 1 }} className="arrow" fontSize="10vh" onClick={() => prevContent()} />
+        <Box sx={{ display: 'inline-flex', width: '90%', height: '100%', flexWrap: 'nowrap', justifyContent: 'space-between', alignItems: 'center', overflow: 'hidden', position: 'relative' }}>
+          {elements.length && elements}
+        </Box>
+        <ArrowForwardIosIcon sx={{ cursor: 'pointer' }} className="arrow" fontSize="10vh" onClick={() => nextContent()} />
       </Box>
     </Box>
   );
